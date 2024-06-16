2d3c78080120b7219f6ec8fffee4a9f8cfa5beb8
Kamal Singh <kamal.singh@exa.net.uk>
2024-06-16 21:38:37 +0100

    use interface to call functions

---
+++ cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"novel-scraper-bot/interanl/scrappers"
+)
+
+func main() {
+	url := "https://novelnextz.com/novelnextz/dragon-marked-war-god#tab-chapters-title"
+	start := 1
+	end := 50
+	novel := scrappers.CreateNovelNextScrapper(url, start, end)
+
+	err := novel.FetchAllLinksOfChapters()
+	if err != nil {
+		slog.Error("in fetching all urls",
+			"error", err,
+		)
+	}
+
+	err = novel.FetchAllChaptersContent()
+	if err != nil {
+		slog.Error("in fetching content for all chapters",
+			"error", err,
+		)
+	}
+
+	m := novel.GetAllChaptersContent()
+	for i := range end - start - 1 {
+		fmt.Println(m[i])
+	}
+}
