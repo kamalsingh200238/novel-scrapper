56405a14aa6e48f90fb425d3bc57344b096d7924
Kamal Singh <kamal.singh@exa.net.uk>
2024-06-17 17:09:14 +0100

    create an epub with the scrapped content

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"log/slog"
 	"novel-scraper-bot/interanl/scrappers"
+
+	"github.com/bmaupin/go-epub"
 )
 
 func main() {
 	url := "https://novelnextz.com/novelnextz/dragon-marked-war-god#tab-chapters-title"
-	start := 1
-	end := 50
+	start := 650
+	end := 700
 	novel := scrappers.CreateNovelNextScrapper(url, start, end)
 
 	err := novel.FetchAllLinksOfChapters()
@@ -27,7 +29,18 @@ func main() {
 	}
 
 	m := novel.GetAllChaptersContent()
-	for i := range end - (start - 1) {
-		fmt.Println(i, m[i])
+	fileName := fmt.Sprintf("Dragon Marked War God (%v - %v)", start, end)
+	e := epub.NewEpub(fileName)
+
+	fmt.Println("Writing in file")
+	for _, v := range m {
+		e.AddSection(v.Content, v.Title, "", "")
 	}
+	err = e.Write(fileName)
+	if err != nil {
+		slog.Error("in writing in the epub file",
+			"error", err,
+		)
+	}
+	fmt.Println("finished writing in file")
 }
