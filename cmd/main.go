7de8bb12b8609837d9c669454f2b54b9debd87ce
Kamal Singh <kamal.singh@exa.net.uk>
2024-09-21 00:03:24 +0100

    rename file

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"log/slog"
-	"novel-scraper-bot/interanl/app"
+	"novel-scraper-bot/internal/app"
 )
 
 func main() {
-	if err := app.MakeEpub("", 1, 500, ""); err != nil {
+	if err := app.MakeEpub("https://novel-next.com/novel/supreme-magus-novel#tab-chapters-title", 1, 50, "temp"); err != nil {
 		slog.Error("error in making epub", "error", err)
 	}
 }
