20ead898d59b712e5a3fb5bf12ef00fe96e35db2
Kamal Singh <kamal.singh@exa.net.uk>
2024-09-26 10:34:35 +0100

    remove novel next because it does not work anymore

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,41 +1,7 @@
 package app
 
-import (
-	"fmt"
-	"log/slog"
-	"novel-scraper-bot/internal/novelnext"
-
-	"github.com/bmaupin/go-epub"
-)
+import "fmt"
 
 func MakeEpub(url string, startChapter int, endChapter int, filename string) error {
-	novel := novelnext.CreateNovelNextScrapper(url, startChapter, endChapter)
-	err := novel.FetchAllLinksOfChapters()
-	if err != nil {
-		return err
-	}
-
-	err = novel.FetchAllChaptersContent()
-	if err != nil {
-		return err
-	}
-
-	m := novel.GetAllChaptersContent()
-	fmt.Println(m)
-
-	fileName := fmt.Sprintf("%s_%d_%d.epub", filename, startChapter, endChapter)
-	novelHeader := fmt.Sprintf("%s_%d_%d", filename, startChapter, endChapter)
-	e := epub.NewEpub(novelHeader)
-
-	slog.Info("writing in file")
-	for _, v := range m {
-		e.AddSection(v.Content, v.Title, "", "")
-	}
-	err = e.Write(fileName)
-	if err != nil {
-	  return err
-	}
-	slog.Info("finished writing in file")
-
-	return nil
+	return fmt.Errorf("novel next does not work anymore")
 }
