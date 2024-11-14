385049b128ff987a77cd37cfde977795b1d643d0
Kamal Singh <kamalsingh200238@gmail.com>
2024-11-14 22:06:10 +0000

    use float instead of int for chapter numbers

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,12 @@
 package app
 
-import "fmt"
+import (
+	"fmt"
+	"novel-scraper-bot/internal/novelnext"
+)
 
-func MakeEpub(url string, startChapter int, endChapter int, filename string) error {
+func MakeEpub(url string, startChapter float64, endChapter float64, filename string) error {
+	novel := novelnext.Init(url, startChapter, endChapter)
+	fmt.Println(novel)
 	return fmt.Errorf("novel next does not work anymore")
 }
