fdcfd31afb1d199ff62a4523a5c4b11a4db354f7
Kamal Singh <kamalsingh200238@gmail.com>
2024-11-17 18:02:52 +0000

    use python script to get content

--- internal/novelnext/novelnext.go
+++ internal/novelnext/novelnext.go
@@ -1,22 +1,75 @@
 package novelnext
 
-import "novel-scraper-bot/internal/scrapper"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"novel-scraper-bot/internal/scrapper"
+	"os/exec"
+	"strings"
+)
 
 type NovelNext struct {
 	startChapter float64
 	endChapter   float64
 	homepageURL  string
+	fileName     string
 	chapters     []scrapper.Chapter
 }
 
-func Init(url string, start float64, end float64) scrapper.Scrapper {
+type ScriptData struct {
+	Data  []scrapper.Chapter `json:"data"`
+	Error string             `json:"error"`
+}
+
+func Init(url string, start float64, end float64, fileName string) scrapper.Scrapper {
 	return &NovelNext{
 		homepageURL:  url,
 		startChapter: start,
 		endChapter:   end,
+		chapters:     []scrapper.Chapter{},
+		fileName:     fileName,
 	}
 }
 
-func (n *NovelNext) FetchAllLinksOfChapters() error            { return nil }
-func (n *NovelNext) FetchAllChaptersContent() error            { return nil }
-func (n *NovelNext) GetAllChaptersContent() []scrapper.Chapter { return []scrapper.Chapter{} }
+func (n *NovelNext) FetchAllChaptersContent() error {
+	script := "scripts/novelnext/fetch-content.py"
+	cmd := exec.Command(
+		".venv/bin/python3",
+		script,
+		"--url", n.homepageURL,
+		"--start", fmt.Sprintf("%f", n.startChapter),
+		"--end", fmt.Sprintf("%f", n.endChapter),
+	)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error in executing python script, error=%v", err)
+	}
+
+	output := out.String()
+	first := strings.Index(output, "{")
+	last := strings.LastIndex(output, "}")
+	if first == -1 || last == -1 {
+		return fmt.Errorf("no json returned")
+	}
+	jsonString := output[first : last+1]
+
+	var data ScriptData
+	if err := json.Unmarshal([]byte(jsonString), &data); err != nil {
+		fmt.Println(err)
+	}
+
+	if data.Error != "" {
+		return fmt.Errorf("error while executing python script, error=%v", data.Error)
+	}
+
+	n.chapters = data.Data
+
+	return nil
+}
+
+func (n *NovelNext) GetAllChaptersContent() []scrapper.Chapter { return n.chapters }
