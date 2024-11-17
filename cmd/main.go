e9aba4ce5dddef2919c86d968f5c7a02cf5c46dd
Kamal Singh <kamalsingh200238@gmail.com>
2024-11-17 18:04:31 +0000

    use the flags to run the binary

--- cmd/main.go
+++ cmd/main.go
@@ -8,19 +8,19 @@ import (
 
 func main() {
 	url := flag.String("url", "", "URL of the novel")
+	startChapter := flag.Float64("start", 0, "starting chapter number")
+	endChapter := flag.Float64("end", 0, "ending chapter number")
 	fileName := flag.String("filename", "", "output filename for the epub")
-	startChapter := flag.Float64("start", 1, "starting chapter number")
-	endChapter := flag.Float64("end", 50, "ending chapter number")
 
 	flag.Parse()
 
 	if err := app.MakeEpub(*url, *startChapter, *endChapter, *fileName); err != nil {
 		slog.Error("error in making epub",
 			"error", err,
-			"url", url,
-			"file name", fileName,
-			"start chatper", startChapter,
-			"end chatper", endChapter,
+			"url", *url,
+			"file name", *fileName,
+			"start chatper", *startChapter,
+			"end chatper", *endChapter,
 		)
 	}
 }
