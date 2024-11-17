764e64e7cb014935bd3d9366ca82806b1e64de47
Kamal Singh <kamalsingh200238@gmail.com>
2024-11-17 18:03:47 +0000

    fetch all chapters content

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,12 +1,13 @@
 package app
 
 import (
-	"fmt"
 	"novel-scraper-bot/internal/novelnext"
 )
 
 func MakeEpub(url string, startChapter float64, endChapter float64, filename string) error {
-	novel := novelnext.Init(url, startChapter, endChapter)
-	fmt.Println(novel)
-	return fmt.Errorf("novel next does not work anymore")
+	novel := novelnext.Init(url, startChapter, endChapter, filename)
+	if err := novel.FetchAllChaptersContent(); err != nil {
+		return err
+	}
+	return nil
 }
